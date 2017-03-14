01e369f3f3eaefadbab52cf317d2e8c176a7d65a
Jainish Shah <Jainishshah@yahoo.com>
2017-03-14 15:56:42 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 echo "loging into docker private registry."
 docker login -u admin -p password 35.185.192.88:5001
 echo "building docker image."
-docker build -t 35.185.192.88:5001/helloworld .
+docker build -t 35.185.192.88:5001/helloworld:$BUILD_NUMBER .
 echo "pushing docker image to private registry."
-docker push 35.185.192.88:5001/helloworld
+docker push 35.185.192.88:5001/helloworld:$BUILD_NUMBER
 echo "removing docker image."
-docker rmi -f 35.185.192.88:5001/helloworld
+docker rmi -f 35.185.192.88:5001/helloworld:$BUILD_NUMBER
